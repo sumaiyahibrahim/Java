e75c2492a67b05de2446d6ed79a1d1870187148b
sumaiyahibrahim <sumaiyahibrahim15@gmail.com>
2025-08-05 23:29:29 +0530

    day 2 commited

---
+++ day2/operators.java
@@ -0,0 +1,11 @@
+package day2;
+
+public class operators {
+    public static void main(String[] args) {
+        // operators in Java
+        // operators are special symbols that perform operations on variables and values
+
+        // Arithmetic Operators
+        // arithmetic operators are used to perform basic mathematical operations
+    }
+}
